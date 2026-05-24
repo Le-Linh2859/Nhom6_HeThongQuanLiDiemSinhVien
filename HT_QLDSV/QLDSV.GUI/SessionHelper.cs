7ae4e713d9d65e0ac17b8f89c048a89f1bc1dfb0
Your Name <your.email@example.com>
2026-05-24 14:49:11 +0700

    up

---
+++ HT_QLDSV/QLDSV.GUI/SessionHelper.cs
@@ -0,0 +1,9 @@
+namespace QLDSV.GUI
+{
+    public static class SessionHelper
+    {
+        public static string MaTaiKhoan { get; set; }
+        public static string TenDangNhap { get; set; }
+        public static string MaVaiTro { get; set; }
+    }
+}
