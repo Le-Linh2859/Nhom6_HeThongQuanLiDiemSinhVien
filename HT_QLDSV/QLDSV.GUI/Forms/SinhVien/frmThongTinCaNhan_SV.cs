8b182d88d53b7710263a7f7de2b4484580f46776
Linhlenezzz <lelinh282005@gmail.com>
2026-05-28 02:49:39 +0700

    Add files via upload
    Giao dien TTCN cho SV

---
+++ HT_QLDSV/QLDSV.GUI/Forms/SinhVien/frmThongTinCaNhan_SV.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLDSV.GUI.Forms.SinhVien
+{
+    public partial class frmThongTinCaNhan_SV : Form
+    {
+        public frmThongTinCaNhan_SV()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonTableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void kryptonTableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
